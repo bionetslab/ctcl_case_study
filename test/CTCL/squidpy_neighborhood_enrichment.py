39278eb00635074645a284c7019d105dbba6ceda
suryadipto <ssarkarmanipal@gmail.com>
2024-02-20 13:21:40 +0100

    ---

--- test/CTCL/squidpy_neighborhood_enrichment.py
+++ test/CTCL/squidpy_neighborhood_enrichment.py
@@ -6,10 +6,12 @@ import scanpy as sc
 import squidpy as sq
 import numpy as np
 import itertools as itt
+import pandas as pd
 
 
 if __name__ == '__main__':
     cnt=-1
+    sq_ne_results=[]
     for filename in os.listdir('data'):
         filename = os.fsdecode(filename)
         if filename.endswith('.h5ad'):
@@ -24,63 +26,31 @@ if __name__ == '__main__':
             celltypes=list(sorted(set(adata.obs['celltype'])))
             cluster_pairs_list=list(itt.combinations(celltypes, 2))
             # ---
-            
-            # ---------------------*************************---------------------
-            print(i)
-            # squidpy.gr.spatial_neighbors(adata)
-            squidpy.gr.spatial_neighbors(adata, coord_type='generic', delaunay=True)
-            # squidpy.gr.spatial_neighbors(adata, coord_type='generic', delaunay=False, n_neighs=6)
-            nhood_enrichment=squidpy.gr.nhood_enrichment(adata, cluster_key='celltype', copy=True, backend="multiprocessing", show_progress_bar=False)
-            # squidpy.gr.nhood_enrichment(adata, cluster_key='celltype', backend="multiprocessing", show_progress_bar=False)
-            # sq.pl.nhood_enrichment(adata, cluster_key="celltype")
-            nhood_enrichment_zscore=nhood_enrichment[0]
-            # nhood_enrichment_zscores.append(nhood_enrichment_zscore)
-            # ---
-            nhood_enrichment_count=nhood_enrichment[1]
-            upper_zscore_matrix = np.triu(np.array(nhood_enrichment_zscore), 1)
-            zscore_list=[]
-            for j in upper_zscore_matrix:
-                for k in j:
-                    if k!=0 and k!=None:
-                        zscore_list.append(k)
-            upper_count_matrix = np.triu(np.array(nhood_enrichment_count), 1)
-            count_list=[]
-            for j in upper_count_matrix:
-                for k in j:
-                    if k!=0 and k!=None:
-                        count_list.append(k)
-            dict_nhoodEnrichment_zscore=dict(zip(cluster_pairs_list, zscore_list))
-            dict_nhoodEnrichment_count=dict(zip(cluster_pairs_list, count_list))
-            # ---
-            Dict_NhoodEnrichment_Zscore.append(dict_nhoodEnrichment_zscore)
-            Dict_NhoodEnrichment_Count.append(dict_nhoodEnrichment_count)
-            # ---
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            if count>0:
-                break
-            adata = sc.read_h5ad('data/'+filename)
-            
             sq.gr.spatial_neighbors(adata, coord_type='generic', delaunay=True)
             nhood_enrichment=sq.gr.nhood_enrichment(adata, cluster_key='celltype', copy=True, backend="multiprocessing", show_progress_bar=False)
             nhood_enrichment_zscore=nhood_enrichment[0]
             nhood_enrichment_count=nhood_enrichment[1]
+            n_shape=np.shape(nhood_enrichment_zscore)[0]
+            upper_zscore_matrix = np.triu(np.array(nhood_enrichment_zscore), 1)
+            zscore_list=[]
+            for j in range(1, n_shape):
+                for k in range(j, n_shape):
+                    zscore_list.append(upper_zscore_matrix[j,k])
             
+            upper_count_matrix = np.triu(np.array(nhood_enrichment_count), 1)
+            count_list=[]
+            for j in range(1, n_shape):
+                for k in range(j, n_shape):
+                    count_list.append(upper_count_matrix[j,k])
             
+            df=pd.DataFrame(cluster_pairs_list, columns =['celltype_1', 'celltype_2'])
+            df['sq_ne_zscore']=zscore_list
+            df['sq_ne_count']=count_list
+            sq_ne_results.append(df)
             
-            
-            # start = time.time()
-            # all_scores(adata, cluster_key='celltype', radii=[1, 2, 3, 4, 5], num_cell_types=11)
-            # end = time.time()
-            # adata.uns['SHouT_execution_time'] = end - start
-            # adata.write_h5ad(os.path.join('results', filename))
+    sq_ne_results=pd.concat(sq_ne_results, axis=0)
+    
+    
+    
+    
+    
