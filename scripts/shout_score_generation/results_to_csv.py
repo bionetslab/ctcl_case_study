78a0eea713fa617e04fb2528a0f656f151d2319f
suryadipto <ssarkarmanipal@gmail.com>
2024-08-11 23:42:49 +0200

    ---

--- scripts/shout_score_generation/results_to_csv.py
+++ scripts/shout_score_generation/results_to_csv.py
@@ -6,6 +6,7 @@ sys.path.insert(0, '../../')
 
 
 if __name__ == '__main__':
+    graph_types=['delaunay_graph', 'knn_graph', 'dist_thresh_graph']
     sample_results = {
         'sample_id': [],
         'patient_id': [],
@@ -15,10 +16,17 @@ if __name__ == '__main__':
         'number_of_cells': [],
         'SHouT_execution_time': [],
         'global_entropy': [],
-        'global_homophily': []
+        'global_homophily_delaunay_graph': [],
+        'global_homophily_knn_graph': [],
+        'global_homophily_dist_thresh_graph': []
     }
+    score_types = ['local_entropy', 'egophily']
+    cell_results = {f'{score_type}_{radius}': [] for score_type in score_types for radius in [1, 2, 3, 4, 5, 10, 20, 50, 100, 500]}
+    score_types=[]
+    for graph_type in graph_types:
+        for radius in [1, 2, 3, 4, 5, 10, 20, 50, 100, 500]:
+            cell_results[f'local_homophily_{radius}_{graph_type}']=[]
     score_types = ['local_homophily', 'local_entropy', 'egophily']
-    cell_results = {f'{score_type}_{radius}': [] for score_type in score_types for radius in [1, 2, 3, 4, 5]}
     cell_results['cell_type'] = []
     cell_results['condition'] = []
     cell_results['sample_id'] = []
@@ -39,7 +47,11 @@ if __name__ == '__main__':
             sample_results['number_of_cells'].append(number_of_cells)
             sample_results['age'].append(int(adata.uns['Age'][0]))
             sample_results['sex'].append(adata.uns['Sex'][0])
-            for shout_result in ['global_entropy', 'global_homophily', 'SHouT_execution_time']:
+            shout_results_list=['global_entropy']
+            for graph_type in graph_types:
+                shout_results_list.append(f'global_homophily_{graph_type}')
+            shout_results_list.append('SHouT_execution_time')
+            for shout_result in shout_results_list:
                 sample_results[shout_result].append(adata.uns[shout_result])
             # Update cell results
             cell_results['condition'] += [condition] * number_of_cells
@@ -47,8 +59,12 @@ if __name__ == '__main__':
             cell_results['patient_id'] += [patient_id] * number_of_cells
             cell_results['cell_type'] += list(adata.obs['celltype'])
             for score_type in score_types:
-                for radius in [1, 2, 3, 4, 5]:
-                    cell_results[f'{score_type}_{radius}'] += list(adata.obs[f'{score_type}_{radius}'])
+                for radius in [1, 2, 3, 4, 5, 10, 20, 50, 100, 500]:
+                    if score_type=='local_homophily':
+                        for graph_type in graph_types:
+                            cell_results[f'{score_type}_{radius}_{graph_type}'] += list(adata.obs[f'{score_type}_{radius}_{graph_type}'])
+                    else:
+                        cell_results[f'{score_type}_{radius}'] += list(adata.obs[f'{score_type}_{radius}'])
     sample_results = pd.DataFrame(data=sample_results)
     cell_results = pd.DataFrame(data=cell_results)
     sample_results.to_csv(os.path.join('../../results', 'sample_results.csv'))
